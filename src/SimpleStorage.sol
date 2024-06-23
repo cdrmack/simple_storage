30f814720d38eb29b7cf13c8da1f5907a83f430d
cdrmack <git.pynvt@passmail.net>
2024-06-23 19:20:26 +0200

    add SimpleStorage

---
+++ src/SimpleStorage.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract SimpleStorage {
+    int32 favoriteNumber;
+
+    struct Person {
+        string name;
+        int32 favoriteNumber;
+    }
+
+    Person[] public people;
+    mapping(string => int32) public nameToFavoriteNumber;
+
+    function addPerson(string memory _name, int32 _favoriteNumber) public {
+        people.push(Person(_name, _favoriteNumber));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+
+    function store(int32 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function get() public view returns (int32) {
+        return favoriteNumber;
+    }
+}
